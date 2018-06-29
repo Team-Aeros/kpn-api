53b31aa3259068e6b607e6c218274e637f2b8e30
Robert Monden <dev@robertmonden.com>
2018-06-29 15:41:09 +0200

    Soap-related stuff

---
+++ app/Http/Controllers/SoapController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \SoapClient;
+use Illuminate\Http\Request;
+
+class SoapController extends Controller
+{
+    public function show()
+    {
+        $client = new SoapClient('http://standardlogics.com/FrontDesk.asmx?wsdl');
+
+        $response = $client->__soapCall("GetCustomerDetails", []);
+        dd($response);
+    }
+}

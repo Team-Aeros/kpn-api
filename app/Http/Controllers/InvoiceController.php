2f5750af8386321762f3056cedc6a126c5956dc8
jhhaanstra <janhendrikhaanstra@gmail.com>
2018-06-29 18:26:37 +0200

    stuff

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        Invoice::create($request->all());
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Invoice::all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(Invoice::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $invoice = Invoice::findOrFail($id);
+        $invoice->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Invoice $invoice
+     * @return void
+     */
+    public function destroy(Invoice $invoice)
+    {
+        $invoice->destroy($invoice->id);
+    }
 }
